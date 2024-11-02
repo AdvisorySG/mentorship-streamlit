cd466dfd9f2c1e41a04a76b6a8bc014027eb74a6
Ng Wei En <weien1292@gmail.com>
2024-11-02 20:43:03 +0800

    [DT-38] chore: migrate to native Streamlit pages (#27)

--- pages/application.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Application Analysis", page_icon="📄", layout="wide")
-
-st.title('Application Analysis Dashboard')
---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit import Page
+
+# from st_pages import Page, show_pages, add_page_title
+
+# https://blog.streamlit.io/crafting-a-dashboard-app-in-python-using-streamlit/ a good reference to dashboards in Streamlit
+
+pg = st.navigation(
+    [
+        Page("pages/home.py", title="Home", icon="🏠"),
+        Page("pages/detailed_info.py", title="Detailed Information", icon="🌐"),
+    ]
+)
+st.set_page_config(page_title="Streamlit Dashboard", page_icon="📈", layout="wide")
+pg.run()
