e756c8e214cfdf25201a9f06826ee7aec799ccbb
vanessa lai <vlaiyz@gmail.com>
2024-12-23 22:10:17 +0800

    Query for clicks and impressions based on industry

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ pg = st.navigation(
     [
         Page("dashboards/demo.py", title="Demo", icon="🎬"),
         Page("dashboards/detailed_info.py", title="Detailed Information", icon="🌐"),
+        Page("dashboards/clicks_and_impressions.py", title="Clicks & Impressions", icon="")
     ]
 )
 pg.run()
