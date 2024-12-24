4b5d6bd7e6b46d651c59b34b7a9a57f48623b7d1
Ng Wei En <weien1292@gmail.com>
2024-12-24 07:25:55 +0000

    feat(dashboards): add avg_screentime
    Co-authored-by: bljr07 <beyblade1704@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from streamlit import Page
 
 pg = st.navigation(
     [
+        Page("dashboards/avg_screentime.py", title="Average Screentime", icon="🖥️"),
         Page("dashboards/demo.py", title="Demo", icon="🎬"),
         Page("dashboards/detailed_info.py", title="Detailed Information", icon="🌐"),
     ]
