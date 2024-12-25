8e1e5c5d66472056666c2e855aa536297f4ed6bc
Ng Wei En <weien1292@gmail.com>
2024-12-25 12:09:39 +0000

    more modifications

--- dashboards/duckdb_shell.py
+++ dashboards/duckdb_shell.py
@@ -14,18 +14,20 @@ st.write("Code adapted from [ducklit](https://github.com/MarkyMan4/ducklit).")
 st.write("Ctrl+enter to run the SQL commands.")
 res = code_editor("SHOW ALL TABLES;", lang="sql", allow_reset=True, key="editor")
 
-st.write("## Queries")
-for query in res["text"].split(";"):
-    if query.strip() == "":
-        continue
+queries = [query for query in res["text"].split(";") if query.strip() != ""]
 
-    st.code(query, "sql")
-    try:
-        start = time.time()
-        cur.execute(query)
-        df = cur.pl()
-        end = time.time()
-        st.write(df)
-        st.write(f"Time taken: {end - start} s")
-    except Exception:
-        st.code(traceback.format_exc())
+if len(queries) > 0:
+    st.write("## Queries")
+
+    for query in queries:
+        st.code(query, "sql")
+        try:
+            start = time.time()
+            cur.execute(query)
+            df = cur.pl()
+            end = time.time()
+
+            st.write(df)
+            st.write(f"Time taken: {end - start} s")
+        except Exception:
+            st.code(traceback.format_exc())
