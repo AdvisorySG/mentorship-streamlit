5296996667efa3d39ca97f70acad0529ad63684e
Ng Wei En <weien1292@gmail.com>
2024-12-25 09:56:12 +0000

    enhance some code editor features

--- dashboards/duckdb_shell.py
+++ dashboards/duckdb_shell.py
@@ -1,6 +1,7 @@
 from code_editor import code_editor
 import streamlit as st
 
+import time
 import traceback
 
 from utils.duckdb import get_dbcur
@@ -13,13 +14,18 @@ st.write("Code adapted from [ducklit](https://github.com/MarkyMan4/ducklit).")
 st.write("Ctrl+enter to run the SQL commands.")
 res = code_editor("SHOW ALL TABLES;", lang="sql", allow_reset=True, key="editor")
 
+st.write("## Queries")
 for query in res["text"].split(";"):
     if query.strip() == "":
         continue
 
+    st.code(query, "sql")
     try:
+        start = time.time()
         cur.execute(query)
         df = cur.pl()
+        end = time.time()
         st.write(df)
+        st.write(f"Time taken: {end - start}s")
     except Exception:
         st.code(traceback.format_exc())
