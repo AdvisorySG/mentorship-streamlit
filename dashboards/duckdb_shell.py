a72fac3fc7bee95a780312d3e03bf1dc874d5344
Jared <jach2355@gmail.com>
2025-02-09 16:18:01 +0800

    feat(duckdb_shell): expand initial query + 2dp time (#54)

--- dashboards/duckdb_shell.py
+++ dashboards/duckdb_shell.py
@@ -10,9 +10,26 @@ st.set_page_config(layout="wide")
 
 cur = get_dbcur()
 
+COMBINED_QUERY = """
+SHOW ALL TABLES;
+
+SELECT COUNT(*) FROM elasticsearch;
+SELECT COUNT(*) FROM umamidb.umami.event_data;
+SELECT COUNT(*) FROM umamidb.umami.session;
+SELECT COUNT(*) FROM umamidb.umami.session_data;
+SELECT COUNT(*) FROM umamidb.umami.website;
+SELECT COUNT(*) FROM umamidb.umami.website_event;
+
+SELECT * FROM elasticsearch LIMIT 1000;
+SELECT * FROM umamidb.umami.event_data LIMIT 1000;
+SELECT * FROM umamidb.umami.session LIMIT 1000;
+SELECT * FROM umamidb.umami.session_data LIMIT 1000;
+SELECT * FROM umamidb.umami.website LIMIT 1000;
+SELECT * FROM umamidb.umami.website_event LIMIT 1000;"""
+
 st.write("Code adapted from [ducklit](https://github.com/MarkyMan4/ducklit).")
 st.write("Ctrl+enter to run the SQL commands.")
-res = code_editor("SHOW ALL TABLES;", lang="sql", allow_reset=True, key="editor")
+res = code_editor(COMBINED_QUERY, lang="sql", allow_reset=True, key="editor")
 
 queries = [query for query in res["text"].split(";") if query.strip() != ""]
 
@@ -28,6 +45,6 @@ if len(queries) > 0:
             end = time.time()
 
             st.write(df)
-            st.write(f"Time taken: {end - start} s")
+            st.write(f"Time taken: {round(end - start, 2)} s") 
         except Exception:
             st.code(traceback.format_exc())
