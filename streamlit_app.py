20e4a1f0af52f94d222c6665f64270fe2cf307a4
Jared <jach2355@gmail.com>
2025-03-14 23:58:19 +0800

    ES EDA (#63)
    * duckdb generic query n time 2dp
    
    * es eda init
    
    * more eda

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,12 @@ pg = st.navigation(
         Page("dashboards/avg_screentime.py", title="Average Screentime", icon="🖥️"),
         Page("dashboards/demo.py", title="Demo", icon="🎬"),
         Page("dashboards/detailed_info.py", title="Detailed Information", icon="🌐"),
-        Page("dashboards/clicks_and_impressions.py", title="Clicks & Impressions", icon=""),
+        Page(
+            "dashboards/clicks_and_impressions.py",
+            title="Clicks & Impressions",
+            icon="",
+        ),
+        Page("dashboards/es_eda.py", title="ES EDA", icon="🌐"),
         Page("dashboards/duckdb_shell.py", title="DuckDB Shell", icon=""),
     ]
 )
