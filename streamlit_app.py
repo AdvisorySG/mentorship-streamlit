481680568d570fa57580be4202e652cc5c9cf19b
Brian Leong Jie Ren <beyblade1704@gmail.com>
2025-06-13 11:42:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ pg = st.navigation(
         Page("dashboards/avg_screentime.py", title="Average Screentime", icon="🖥️"),
         Page("dashboards/demo.py", title="Demo", icon="🎬"),
         Page("dashboards/detailed_info.py", title="Detailed Information", icon="🌐"),
-        Page("dashboards/detailed_info_2.py", title="Detailed Information 2", icon="🌐"),
-        Page("dashboards/detailed_info_3.py", title="Detailed Information 3", icon="🌐"),
+        Page("dashboards/visits_filter.py", title="Visits Filter View", icon="🌐"),
         Page("dashboards/duckdb_shell.py", title="DuckDB Shell", icon=""),
     ]
 )
